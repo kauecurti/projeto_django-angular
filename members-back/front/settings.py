f69e5ba6a3875f4f05b031c201fb821c136aebaa
kaue <kauecurti@hotmail.com>
2020-06-18 12:27:52 -0300

    banco de dados

--- members-back/front/settings.py
+++ members-back/front/settings.py
@@ -87,20 +87,19 @@ WSGI_APPLICATION = 'front.wsgi.application'
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
 DATABASES = {
-     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'djangodb',
         'USER': 'django',
         'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
+        'HOST': 'localhost',
+        'PORT': '',
+    },
     'local': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-password-validators
